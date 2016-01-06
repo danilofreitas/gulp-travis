454231d6297a8e7f9b5b8880df18d1b83a9eab7a
danilofreitas <danilogomesf@gmail.com>
2016-01-06 11:07:34 +0100

    first commit

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('angularTravis', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'restangular', 'ui.router', 'toastr']);
+
+})();
